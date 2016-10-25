28fa580aa6a2e783a6dba7d28ac3c48b5a483545
Domiii <dominik.seifert@gmail.com>
2016-10-25 17:34:44 +0800

    - added automatic main menu building; - fixed targeting bug

---
+++ Assets/Scripts/Editor/MainMenuEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MainMenu))]
+public class MainMenuEditor : Editor {
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI ();
+
+		var menu = (MainMenu)target;
+
+		if (GUILayout.Button ("Clear Menu")) {
+			menu.ClearMenu ();
+		}
+
+		if (GUILayout.Button ("Build Menu")) {
+			menu.BuildMenu ();
+		}
+	}
+}
--- Assets/Scripts/InputControl/WASDClickToShootPlayerControl.cs
+++ Assets/Scripts/InputControl/WASDClickToShootPlayerControl.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class WASDClickToShootPlayerControl : PlayerControlBase {
 	public float speed = 8;
 	bool mouseDown;
+	Plane testPlane;
 
 	public bool IsOnNavMesh(out NavMeshHit hit) {
 		return NavMesh.Raycast (transform.position, transform.position + Vector3.down * 100, out hit, NavMesh.AllAreas);
+	}
+
+	void Start() {
+		// create a plane at 0,0,0 whose normal points to +Y
+		testPlane = new Plane ();
 	}
 
 	void Update() {
@@ -58,14 +64,13 @@ public class WASDClickToShootPlayerControl : PlayerControlBase {
 	}
 
 	void HandleMouse(bool clicked) {
-		Ray screenRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-		RaycastHit hit;
-		if (Physics.Raycast(screenRay, out hit))
-		{
-			//HuntOrMoveToDestination (hit, clicked);
-			var target = hit.point;
-			target.y = transform.position.y;
+		// see: http://answers.unity3d.com/questions/269760/ray-finding-out-x-and-z-coordinates-where-it-inter.html
+		// cast ray onto plane that goes through our current position and normal points upward
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		testPlane.SetNormalAndPosition (Vector3.up, transform.position);
+		float distance; 
+		if (testPlane.Raycast(ray, out distance)){
+			var target = ray.GetPoint(distance);
 			NextAction = new Strategies.ShootInDirectionAction {
 				destination = target
 			};
